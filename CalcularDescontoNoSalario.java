cf65524195d8b3b85c976028774b01fd83af239f
Jean C Miguel <fullstack.jeancmiguel@gmail.com>
2025-09-16 14:24:55 -0300

    Add files via upload

---
+++ CalcularDescontoNoSalario.java
@@ -0,0 +1,8 @@
+public class CalcularDescontoNoSalario {
+    public static void main(String[] args){
+        float salarioBruto = 4568.00F;
+        float porcentagemDoSalario = 16;
+        float porcentagem = salarioBruto * (porcentagemDoSalario/ 100);
+        System.out.println(porcentagem);
+    }
+}
